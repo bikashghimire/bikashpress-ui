5daaafde921321ed35d08d4e7a8bdaca1a672531
bikashghimire <bikashghimire2610@gmail.com>
2024-06-08 13:19:08 +0300

    added themeprovider for light and dark theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
 import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
+import { ThemeProvider } from "@/components/providers/ThemeProvider";
 
 export const metadata: Metadata = {
   title: "Bikash Press",
@@ -19,14 +20,21 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        <div className="flex">
-          <div className="hidden md:block h-[100vh] w-[300px]">
-            <Sidebar />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          enableSystem
+          storageKey="dashboard-theme"
+        >
+          <Navbar />
+          <div className="flex">
+            <div className="hidden md:block h-[100vh] w-[300px]">
+              <Sidebar />
+            </div>
+            <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
           </div>
-          <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
-        </div>
-        <Toaster />
+          <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
