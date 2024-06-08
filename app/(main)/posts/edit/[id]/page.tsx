caa8fe15b3442e5e5342baf587c8167c8ff68b21
bikashghimire <bikashghimire2610@gmail.com>
2024-06-08 12:52:50 +0300

    added toaster

--- app/(main)/posts/edit/[id]/page.tsx
+++ app/(main)/posts/edit/[id]/page.tsx
@@ -6,22 +6,31 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
-  FormField,
-  FormLabel,
-  FormItem,
-  FormMessage,
   FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 import posts from "@/data/posts";
+import { useToast } from "@/components/ui/use-toast";
 
 const formSchema = z.object({
-  title: z.string().min(1, "Title is required"),
-  body: z.string().min(1, "Body is required"),
-  author: z.string().min(1, "Author is required"),
-  date: z.string().min(1, "Date is required"),
+  title: z.string().min(1, {
+    message: "Title is required",
+  }),
+  body: z.string().min(1, {
+    message: "Body is required",
+  }),
+  author: z.string().min(1, {
+    message: "Author is required",
+  }),
+  date: z.string().min(1, {
+    message: "Date is required",
+  }),
 });
 
 interface PostEditPageProps {
@@ -31,6 +40,8 @@ interface PostEditPageProps {
 }
 
 const PostEditPage = ({ params }: PostEditPageProps) => {
+  const { toast } = useToast();
+
   const post = posts.find((post) => post.id === params.id);
 
   const form = useForm<z.infer<typeof formSchema>>({
@@ -44,12 +55,15 @@ const PostEditPage = ({ params }: PostEditPageProps) => {
   });
 
   const handleSubmit = (data: z.infer<typeof formSchema>) => {
-    console.log(data);
+    toast({
+      title: "Post has been updated successfully",
+      description: `Updated by ${post?.author} on ${post?.date}`,
+    });
   };
 
   return (
     <>
-      <BackButton text="Back to Posts" link="/posts" />
+      <BackButton text="Back To Posts" link="/posts" />
       <h3 className="text-2xl mb-4">Edit Post</h3>
       <Form {...form}>
         <form onSubmit={form.handleSubmit(handleSubmit)} className="space-y-8">
@@ -58,12 +72,12 @@ const PostEditPage = ({ params }: PostEditPageProps) => {
             name="title"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
                   Title
                 </FormLabel>
                 <FormControl>
                   <Input
-                    className="bg-slate-100 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                    className="bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible: ring-offset-0"
                     placeholder="Enter Title"
                     {...field}
                   />
@@ -72,17 +86,18 @@ const PostEditPage = ({ params }: PostEditPageProps) => {
               </FormItem>
             )}
           />
+
           <FormField
             control={form.control}
             name="body"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
                   Body
                 </FormLabel>
                 <FormControl>
                   <Textarea
-                    className="bg-slate-100 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                    className="bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible: ring-offset-0"
                     placeholder="Enter Body"
                     {...field}
                   />
@@ -91,17 +106,18 @@ const PostEditPage = ({ params }: PostEditPageProps) => {
               </FormItem>
             )}
           />
+
           <FormField
             control={form.control}
             name="author"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
                   Author
                 </FormLabel>
                 <FormControl>
                   <Input
-                    className="bg-slate-100 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                    className="bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible: ring-offset-0"
                     placeholder="Enter Author"
                     {...field}
                   />
@@ -110,17 +126,18 @@ const PostEditPage = ({ params }: PostEditPageProps) => {
               </FormItem>
             )}
           />
+
           <FormField
             control={form.control}
             name="date"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70">
+                <FormLabel className="uppercase text-xs font-bold text-zinc-500 dark:text-white">
                   Date
                 </FormLabel>
                 <FormControl>
                   <Input
-                    className="bg-slate-100 border-0 focus-visible:ring-0 text-black focus-visible:ring-offset-0"
+                    className="bg-slate-100 dark:bg-slate-500 border-0 focus-visible:ring-0 text-black dark:text-white focus-visible: ring-offset-0"
                     placeholder="Enter Date"
                     {...field}
                   />
@@ -129,7 +146,10 @@ const PostEditPage = ({ params }: PostEditPageProps) => {
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+
+          <Button className="w-full dark:bg-slate-800 dark: text-white">
+            Update Post
+          </Button>
         </form>
       </Form>
     </>
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Sidebar from "@/components/Sidebar";
-
+import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,12 +19,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Navbar />
         <div className="flex">
           <div className="hidden md:block h-[100vh] w-[300px]">
             <Sidebar />
           </div>
           <div className="p-5 w-full md:max-w-[1140px]">{children}</div>
         </div>
+        <Toaster />
       </body>
     </html>
   );
